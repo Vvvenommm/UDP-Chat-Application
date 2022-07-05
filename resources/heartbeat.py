ca559412a775e3a72120a7d93de81fa28252c6eb
Mustafa Nail Bülbül <buelbuel@outlook.de>
2022-07-05 09:03:36 +0200

    Adjust heartbeat to recognize Server crash

--- resources/heartbeat.py
+++ resources/heartbeat.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 from time import sleep
-from resources import utils
+from resources import utils, leader_election
 
 
 def start_heartbeat_listener():
@@ -11,10 +11,11 @@ def start_heartbeat_listener():
         neigbour_crash_count = 0
         is_crashed = False
         # get own Server Neighbour by using Leader Election algorithm
-        host_address = (utils.neighbour, utils.SERVER_PORT)
+        #host_address = (utils.neighbour, utils.SERVER_PORT)
         # only executed if a Neighbour is available to whom the Server can establish a connection
-        if host_address:
-
+        if utils.neighbour:
+            #print('Iam here')
+            neighbour_failed = False
             # create the UDP Socket for Heartbeat
             heartbeat_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -27,26 +28,22 @@ def start_heartbeat_listener():
 
             heartbeat_socket.sendto(b'PING', (utils.neighbour, utils.HEARTBEAT_PORT))
 
-            while True:
+            while not neighbour_failed:
                 try:
                     data, addr = heartbeat_socket.recvfrom(1024)
                     if data:
-                        sleep(3)
+                        sleep(1)
                         print(f'[HEARTBEAT] - Neighbour {utils.neighbour} response',
                               file=sys.stderr)
                         heartbeat_socket.sendto(b'PING', addr)
                         neigbour_crash_count = 0
-                    else:
-                        print('Closing receive')
-                        heartbeat_socket.close()
-                        break
 
                 except Exception as e:
                     sleep(3)
                     print(f'[HEARTBEAT] - connection lost to Neighbour: {utils.neighbour} with exception: {e}')
                     neigbour_crash_count += 1
 
-                    if neigbour_crash_count > 5:
+                    if neigbour_crash_count > 3:
                         # remove crashed Neighbour from Server List
                         utils.SERVER_LIST.remove(utils.neighbour)
 
@@ -59,20 +56,39 @@ def start_heartbeat_listener():
                             # assign own IP address as new Server Leader
                             utils.leader = utils.myIP
                             utils.network_changed = True
-                            heartbeat_socket.close()
-                            break
+                            neighbour_failed = True
 
                         # used if crashed Neighbour was a Server Replica
                         else:
                             print(f'[HEARTBEAT] - Server Replica {utils.neighbour} crashed',
                                   file=sys.stderr)
                             utils.replica_crashed = True
-                            heartbeat_socket.close()
-                            break
+                            neighbour_failed = True
 
-                        break
+            print('Out')
+            utils.neighbour = ''
 
-                else:
-                    print('Closing')
-                    heartbeat_socket.close()
-                    break
+def test():
+        sleep(3)
+        print(f'[HEARTBEAT] - connection lost to Neighbour: {utils.neighbour} with exception: {e}')
+        #neigbour_crash_count += 1
+
+        #if neigbour_crash_count > 5:
+        # remove crashed Neighbour from Server List
+        utils.SERVER_LIST.remove(utils.neighbour)
+
+        # used if the crashed Neighbour was the Server Leader
+        if utils.leader == utils.neighbour:
+            print(f'[HEARTBEAT] - Server Leader {utils.neighbour} crashed',
+                  file=sys.stderr)
+            utils.leader_crashed = True
+
+            # assign own IP address as new Server Leader
+            utils.leader = utils.myIP
+            utils.network_changed = True
+
+        # used if crashed Neighbour was a Server Replica
+        else:
+            print(f'[HEARTBEAT] - Server Replica {utils.neighbour} crashed',
+                  file=sys.stderr)
+            utils.replica_crashed = True
