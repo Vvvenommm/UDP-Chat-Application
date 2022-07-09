5b1ecf51bc3b1062fb51225f070999e782028bae
Mustafa Nail Bülbül <buelbuel@outlook.de>
2022-07-09 13:55:27 +0200

    Add if else for multicast

--- multicast/multicast_receive.py
+++ multicast/multicast_receive.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import struct
 import pickle
@@ -10,8 +11,12 @@ multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #source: https://pymotw.com/3/socket/multicast.html
 def start_receiver():
+    if sys.platform == 'win32':
+      multicast_socket.bind(('', utils.MULTICAST_PORT))
+    else:
+      multicast_socket.bind((utils.myIP, utils.MULTICAST_PORT))
     # Bind to the server address
-    multicast_socket.bind(('', utils.MULTICAST_PORT))
+    #multicast_socket.bind(('', utils.MULTICAST_PORT))
 
     # Tell the operating system to add the socket to
     # the multicast group on all interfaces.
