c81ecb4130ed6ed393d859408a562e3806856002
Mustafa Nail Bülbül <buelbuel@outlook.de>
2022-07-10 02:36:59 +0200

    Add platform check

--- resources/heartbeat.py
+++ resources/heartbeat.py
@@ -20,6 +20,8 @@ def start_heartbeat_listener():
             # Set the socket to broadcast and enable reusing addresses
             heartbeat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             heartbeat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            if sys.platform is not "win32":
+                heartbeat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
             heartbeat_socket.settimeout(2)
             heartbeat_socket.bind(('', utils.HEARTBEAT_PORT))
