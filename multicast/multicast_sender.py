717d4cb89eb38da6ade5739738d2a27a8b7dda17
Mustafa Nail Bülbül <buelbuel@outlook.de>
2022-07-10 10:56:35 +0200

    Change sleep of multicast

--- multicast/multicast_sender.py
+++ multicast/multicast_sender.py
@@ -9,7 +9,7 @@ from resources import utils
 # Set a timeout so the socket does not block
 # indefinitely when trying to receive data.
 multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-multicast_socket.settimeout(1)
+multicast_socket.settimeout(2)
 
 # Set the time-to-live for messages to 1 so they do not
 # go past the local network segment.
@@ -19,7 +19,7 @@ multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 #source: https://pymotw.com/3/socket/multicast.html
 def start_sender():
 
-    sleep(1)
+    sleep(2)
 
     # Send data to the multicast group
     message = pickle.dumps([utils.RequestType.SERVER_JOIN.value, utils.SERVER_LIST, utils.CLIENT_LIST, utils.leader, ''])
