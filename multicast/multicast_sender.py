6b1d762bfe97ddc28e43479ba9deb6326eb15461
Mustafa Nail Bülbül <buelbuel@outlook.de>
2022-07-10 11:10:48 +0200

    Remove timeoouts

--- multicast/multicast_sender.py
+++ multicast/multicast_sender.py
@@ -12,13 +12,13 @@ def start_sender():
     # Set a timeout so the socket does not block
     # indefinitely when trying to receive data.
     multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    multicast_socket.settimeout(2)
+    multicast_socket.settimeout(1)
 
     # Set the time-to-live for messages to 1 so they do not
     # go past the local network segment.
     ttl = struct.pack('b', 1)
     multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-    sleep(2)
+    sleep(1)
 
     # Send data to the multicast group
     message = pickle.dumps([utils.RequestType.SERVER_JOIN.value, utils.SERVER_LIST, utils.CLIENT_LIST, utils.leader, ''])
